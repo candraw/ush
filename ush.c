9f134de89fb5c53c16c91f1d9951d146374e2025
Jannik Birk <birk.jannik@gmail.com>
2016-10-17 22:09:26 +0200

    refactor

--- ush.c
+++ ush.c
@@ -3,12 +3,22 @@
 #include<stdlib.h>
 #include<sys/wait.h>
 
-char cmd[1024];
+void fork_and_execute();
+void exec_cmd(char*, char *const []);
+
+static char cmd[1024];
 
 int main()
 {
   fscanf(stdin, "%s", &cmd);
   
+  fork_and_execute();
+
+  return 0;
+}
+
+void fork_and_execute()
+{
   int pid = fork();
 
   if ( pid > 0 )
@@ -16,13 +26,17 @@ int main()
     wait(0);
   } else if ( pid == 0 )
   {
-    char *const argv[] = {""};
-    execv("/bin/sh", argv);
+    char *const argv[] = {cmd, (char *)0};
+    exec_cmd(cmd, argv);
   } else
   {
     puts("Fork error!");
     exit(1);
   }
+}
 
-  return 0;
+void exec_cmd(char *cmd, char *const argv[])
+{
+  execv(cmd, argv);
+  printf("Could not execute %s\n", cmd);
 }
